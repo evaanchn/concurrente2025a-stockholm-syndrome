ba4d1896ae940621eb19b88529b33b582f767b39
Evan Chen <evan.chen@ucr.ac.cr>
2025-07-09 21:56:28 -0600

    nbody: initial files

---
+++ nbody/src/main.cpp
@@ -0,0 +1,16 @@
+// Copyright 2025 Stockholm Syndrome. Universidad de Costa Rica. CC BY 4.0
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Start program execution.
+ *
+ * @return Status code to the operating system, 0 means success.
+ */
+int main(void) {
+  // return simulation.run();
+  return EXIT_SUCCESS;
+}
