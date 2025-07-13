0c09238ec94343c2937484925440037bb44490ab
Albin Monge <albin.monge@ucr.ac.cr>
2025-07-13 04:48:21 -0600

    nbody: Simulation: run modularization

--- nbody/src/simulation/Simulation.hpp
+++ nbody/src/simulation/Simulation.hpp
@@ -9,41 +9,62 @@
 #include "Body.hpp"
 #include "common.hpp"
 #include "RealVector.hpp"
-// #include "Statistics.hpp"
 #include "Universe.hpp"
 
 // forward declaration
 class Mpi;
 
+/**
+ * @class Simulation
+ * @brief Manages the simulation process, including the initialization of
+ * bodies, the simulation loop, and the generation of reports.
+ */
 class Simulation {
+  DECLARE_RULE4(Simulation, delete);
+
  private:
+  /// unit of time that increase by state.
   double deltaTime = 0.0;
+  /// maximum time allowed for the simulation.
   double maxTime = 0.0;
+  /// total number of bodies at the start.
   int totalBodiesCount = 0;
+  /// total active bodies in the simulation, currently.
+  int totalActiveBodiesCount = 0;
 
   /// Container for the bodies in the simulation.
   Universe universe;
-  // Statistics statistics;
   /// File containing the universe data.
-  std::string universeFile;
+  std::string universeFile = "";
   /// Distributed MPI object for parallel processing.
   Mpi* mpi = nullptr;
 
  public:
   /// @brief Constructor for the Simulation class.
   Simulation() = default;
-  DECLARE_RULE4(Simulation, delete);
+  /// @brief Destructor of the class
   ~Simulation();
 
+  /// @brief Execute the simulation process.
+  /// @param argc Number of command line arguments.
+  /// @param argv Array of command line arguments.
+  /// @return EXIT_SUCCESS if simulated successfully, else EXIT_FAILURE
   int run(int argc, char* argv[]);
 
  private:
+  /// @brief Analyzes arguments and prepares the universe
+  /// @see run
+  int startSimulation(int argc, char* argv[]);
+  /// @brief Attempts to store specified values in attributes, ensuring
+  /// that these are correct
+  /// @see run for params
+  /// @return The execution mode determined after analyzing the arguments
   ExecutionMode analyzeArguments(int argc, char* argv[]);
+  /// @brief Carries out main simulation loop
+  /// @return The total simulated time
+  double simulate();
 
- private:
-  size_t remaining() const;
-
- private:
+ private:  // STATES FOR SIMULATE
   /// @brief State of the simulation in wich all processes
   // check for collisions from other broadcasted data.
   void stateCollisions();
@@ -55,6 +76,9 @@ class Simulation {
   void statePositions();
 
  private:
+  /// @brief Processes take turns reporting the final states of their universe
+  /// @param simulatedTime Total time simulated
+  void saveFinalState(double simulatedTime);
   /// @brief Generate a report of mean and standard deviation of distances and
   // velocities.
   /// @param totalActiveBodiesCount The total number of active bodies at the
